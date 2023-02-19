f46cd070fa7f03a166eb88a4b77d3c7123e64e57
Natissa-Didy Toure <94816453+Natissa-DidyToure@users.noreply.github.com>
2023-02-18 20:49:19 -0500

    created the css

---
+++ script.js
@@ -0,0 +1,49 @@
+const questionContainers = document.querySelectorAll('.question-container');
+const nextButton = document.querySelector('#next-button');
+let currentQuestionIndex = 0;
+let score = 0;
+
+hideAllQuestions();
+showQuestion(currentQuestionIndex);
+
+function hideAllQuestions() {
+  questionContainers.forEach(container => {
+    container.style.display = 'none';
+  });
+}
+
+function showQuestion(index) {
+  questionContainers[index].style.display = 'block';
+}
+
+function updateScore(isCorrect) {
+  if (isCorrect) {
+    score++;
+  }
+}
+
+function showNextQuestion() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questionContainers.length) {
+    hideAllQuestions();
+    showQuestion(currentQuestionIndex);
+  } else {
+    alert(`Quiz complete! Your score is ${score}/${questionContainers.length}.`);
+  }
+}
+
+questionContainers.forEach(container => {
+  const answerInputs = container.querySelectorAll('input[type="radio"]');
+  answerInputs.forEach(input => {
+    input.addEventListener('click', () => {
+      const isCorrect = input.value === '1';
+      updateScore(isCorrect);
+      nextButton.disabled = false;
+    });
+  });
+});
+
+nextButton.addEventListener('click', () => {
+  showNextQuestion();
+  nextButton.disabled = true;
+});
